748273af38eead95a8064c204f71c0fd7b1c6b09
Fred0525 <1054580932@qq.com>
2022-09-14 23:47:51 +0800

    代码调整

--- source_exchange_rate_grab/currency_list.py
+++ source_exchange_rate_grab/currency_list.py
@@ -1,7 +1,7 @@
 CURRENCY_LIST = [
     "CNY",  # 人民币
     "GBP",  # 英镑
-    "HKD",  # 港元
+    "HKD",  # 港币
     "USD",  # 美元
     "CHF",  # 瑞士法郎
     "FRF",  # 法国法郎
--- source_exchange_rate_grab/source.py
+++ source_exchange_rate_grab/source.py
@@ -1,8 +1,8 @@
 #
 # Copyright (c) 2022 Airbyte, Inc., all rights reserved.
 #
-import json
 import logging
+import time
 from typing import Any, List, Mapping, Tuple, Optional, Iterable, MutableMapping, Union
 
 import requests
@@ -57,29 +57,7 @@ class ExchangeRate(HttpStream):
     def request_headers(
         self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> Mapping[str, Any]:
-        return {
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Accept-Language": "zh-CN,zh;q=0.9",
-            "Cache-Control": "no-cache",
-            "Connection": "keep-alive",
-            "Content-Length": "110",
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Cookie": "JSESSIONID=0000CX6KiWeduTAqThpQ13j8nBs:-1",
-            "Host": "srh.bankofchina.com",
-            "Origin": "https://srh.bankofchina.com",
-            "Pragma": "no-cache",
-            "Referer": "https://srh.bankofchina.com/search/whpj/search_cn.jsp",
-            "sec-ch-ua": '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": '"Windows"',
-            "Sec-Fetch-Dest": "document",
-            "Sec-Fetch-Mode": "navigate",
-            "Sec-Fetch-Site": "same-origin",
-            "Sec-Fetch-User": "?1",
-            "Upgrade-Insecure-Requests": "1",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
-        }
+        return {}
 
     def request_body_data(
             self,
@@ -88,9 +66,10 @@ class ExchangeRate(HttpStream):
             next_page_token: Mapping[str, Any] = None,
     ) -> Optional[Union[Mapping, str]]:
         cur_map = currency_map.CURRENCY_MAP
+        current_time_str = time.strftime("%Y-%m-%d", time.localtime(time.time()))
         return {
-            "erectDate": "2022-09-13",
-            "nothing": "2022-09-13",
+            "erectDate": current_time_str,
+            "nothing": current_time_str,
             "pjname": cur_map.get(self.currency),
             # "page": "2022-09-13",
             "head": "head_620.js",
@@ -112,23 +91,22 @@ class ExchangeRate(HttpStream):
         stream_slice: Mapping[str, Any] = None,
         next_page_token: Mapping[str, Any] = None,
     ) -> Iterable[Mapping]:
-
         html = etree.HTML(response.text)
-        m_n = html.xpath('//tr[{}]/td[1]/text()'.format(2))
-        s_e_p = html.xpath('//tr[{}]/td[2]/text()'.format(2))
-        c_p = html.xpath('//tr[{}]/td[3]/text()'.format(2))
-        s_e_sp = html.xpath('//tr[{}]/td[4]/text()'.format(2))
-        c_s = html.xpath('//tr[{}]/td[5]/text()'.format(2))
+        currency = html.xpath('//tr[{}]/td[1]/text()'.format(2))
+        s_p_p = html.xpath('//tr[{}]/td[2]/text()'.format(2))
+        c_p_p = html.xpath('//tr[{}]/td[3]/text()'.format(2))
+        s_s_p = html.xpath('//tr[{}]/td[4]/text()'.format(2))
+        c_s_p = html.xpath('//tr[{}]/td[5]/text()'.format(2))
         bank_count_p = html.xpath('//tr[{}]/td[6]/text()'.format(2))
-        date = html.xpath('//tr[{}]/td[7]/text()'.format(2))
+        release_time = html.xpath('//tr[{}]/td[7]/text()'.format(2))
 
         data = {
-            "currency": self.parse_data(m_n[0]),
-            "spot_purchase_price": self.parse_data(s_e_p[0]),
-            "cash_purchase_price": self.parse_data(c_p[0]),
-            "spot_sell_price": self.parse_data(s_e_sp[0]),
-            "cash_selling_price": self.parse_data(c_s[0]),
+            "currency": self.parse_data(currency[0]),
+            "spot_purchase_price": self.parse_data(s_p_p[0]),
+            "cash_purchase_price": self.parse_data(c_p_p[0]),
+            "spot_sell_price": self.parse_data(s_s_p[0]),
+            "cash_selling_price": self.parse_data(c_s_p[0]),
             "bank_count_price": self.parse_data(bank_count_p[0]),
-            "release_time": self.parse_data(date[0]),
+            "release_time": self.parse_data(release_time[0]),
         }
         return [data]
